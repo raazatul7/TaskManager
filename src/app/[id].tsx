8961351eebb66f545450792709d7d0ee0c0f4aaa
Atul Gupta <atul.gupta@mprofit.in>
2025-02-21 22:13:45 +0530

    Add task description editing functionality in TaskDetails component

--- src/app/[id].tsx
+++ src/app/[id].tsx
@@ -1,13 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, TextInput } from 'react-native'
+import React, { useState } from 'react'
 import { Stack, useLocalSearchParams } from 'expo-router'
-import { useObject } from '@realm/react';
+import { useObject, useRealm } from '@realm/react';
 import { Task } from '../models/Tasks';
 import { BSON } from 'realm';
 
 const TaskDetails = () => {
     const { id } = useLocalSearchParams();
     const task = useObject<Task>(Task, new BSON.ObjectID(id as string));
+    const realm = useRealm();
+    const [desc, setDesc] = useState(task?.description || '');
+
+    const updateTask = () => {
+        if(!task) {
+            return;
+        }
+        realm.write(() => {
+           task.description = desc;
+        })
+    }
 
     if (!task) {
         return <Text>Task not found</Text>
@@ -16,7 +27,11 @@ const TaskDetails = () => {
     return (
         <View style={{ padding: 10 }}>
             <Stack.Screen options={{ title: 'Task Details' }} />
-            <Text style={{ color: 'white', fontSize: 20 }}>{task.description}</Text>
+            <TextInput style={{ color: 'white', fontSize: 20 }}
+                value={desc}
+                onChangeText={setDesc}
+                onEndEditing={updateTask}
+            />
         </View>
     )
 }
